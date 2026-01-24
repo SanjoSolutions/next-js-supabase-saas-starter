0302fa80695ddaa14ec1cf713b0831a33ceb5855
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2026-01-24 03:47:08 +0100

    Removes "Deploy to Vercel" button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { DeployButton } from "@/components/deploy-button"
-import { EnvVarWarning } from "@/components/env-var-warning"
 import { AuthButton } from "@/components/auth-button"
+import { EnvVarWarning } from "@/components/env-var-warning"
 import { Hero } from "@/components/hero"
 import { ThemeSwitcher } from "@/components/theme-switcher"
 import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps"
@@ -17,9 +16,6 @@ export default function Home() {
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
             </div>
             {!hasEnvVars ? (
               <EnvVarWarning />
