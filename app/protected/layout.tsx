b220cec73e6f369bf14c7a8668ef14206a67665a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2026-01-24 03:30:03 +0100

    Formats files

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,15 +1,15 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import { Suspense } from "react";
+import { DeployButton } from "@/components/deploy-button"
+import { EnvVarWarning } from "@/components/env-var-warning"
+import { AuthButton } from "@/components/auth-button"
+import { ThemeSwitcher } from "@/components/theme-switcher"
+import { hasEnvVars } from "@/lib/utils"
+import Link from "next/link"
+import { Suspense } from "react"
 
 export default function ProtectedLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
@@ -51,5 +51,5 @@ export default function ProtectedLayout({
         </footer>
       </div>
     </main>
-  );
+  )
 }
